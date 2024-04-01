f8dbc0dbd8f3abfe32babee244ecdf942459b34d
MATHEUS RIBEIRO MATOS <matheus.rmatos@senacsp.edu.br>
2024-04-01 16:56:48 -0300

    ssas

---
+++ 4-revisão/JS/script.js
@@ -0,0 +1,87 @@
+// // console.log("ola mundo")
+// console.log('ola mundo 2')
+// console.log(`ola mundo `)
+
+// let nome = 'matheus'
+// const idade = 22
+// var profissao = 'programador'
+// console.log(typeof nome)
+// console.log(typeof idade)
+// console.log(profissao)
+
+// console.log(nome + idade + profissao)
+// console.log("ususario: " + nome + "idade:" + idade + " profissao: " + profissao)
+
+// console.log(`${nome} ${idade} ${profissao}`)
+
+// var idade = parseInt(prompt("digite sua idade"))
+
+// alert("voce tem " + idade + "anos")
+
+// var idade2 = idade + 10
+
+// alert("daqui a 10 anos voce tera " + idade2 + "anos")
+
+// let nome = prompt("digite seu nome")
+// let sobrenome = prompt("sobrenome")
+
+// alert(`o nome completo e: ${nome} ${sobrenome}`)
+
+// let idade = parseInt(prompt("idade"))
+
+// if(idade >= 18) {
+//     alert('pode entrar')
+// }else{
+//     alert('nao pode entrar')
+
+
+// let nome = prompt("digite seu nome")
+// let sobrenome = prompt("sobrenome")
+
+// alert(`o nome completo e: ${nome} ${sobrenome}`)
+
+// let idade = parseInt(prompt("idade"))
+
+// if(idade >= 18) {
+//     alert('pode entrar')
+// }else if (idade >=15 && idade<18){
+//     alert('pode entrar com os pais')
+// }else{
+//     alert('nao pode entrar')
+// }
+
+// for (let i = 0; i <= 50; i++) {
+//     console.log(`o valor de i e: ${i}`)
+// }
+
+// let p = 0;
+
+// while (p < 5){
+//     console.log(`repetindo ${p}`)
+//     p++
+// }
+
+// let g = 10
+
+// do{
+//     console.log(`valor de G: ${g}`)
+//     g = g -1
+// }while(g > 1)
+
+
+//  const trabalho = prompt("digite seu trabalho")
+
+//  switch(trabalho){
+//     case "programador":
+//         console.log("voce gosta de programar")
+//         break;
+//     case "engenheiro":
+//         console.log("voce gosta de calculo")
+//         break;
+//     case "advogado":
+//         console.log("voce gosta de justiça")
+//         break;
+//     default:
+//         console.log("profissao nao encontrada")
+//         break;
+//  }
